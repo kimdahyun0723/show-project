4fef30ebd94b963b29021cef37859ce2bbbc1eed
junseon <kimkva@naver.com>
2024-10-07 12:12:45 +0900

    dependency setting

---
+++ src/main/java/com/keduit/show/service/MemberService.java
@@ -0,0 +1,11 @@
+package com.keduit.show.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MemberService {
+}
