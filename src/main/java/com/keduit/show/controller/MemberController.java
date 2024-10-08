c6ad8b5b509fdf1aa5ae5a8c48f11ea36270a0eb
junseon <kimkva@naver.com>
2024-10-08 15:27:09 +0900

    update member

--- src/main/java/com/keduit/show/controller/MemberController.java
+++ src/main/java/com/keduit/show/controller/MemberController.java
@@ -64,12 +64,6 @@ public class MemberController {
         return "member/loginForm";
     }
 
-    @PostMapping("/login")
-    public String login(@Valid MemberDTO memberDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-
-        return  "redirect:/";
-    }
-
     @GetMapping("/login/error")
     public  String loginError(Model model) {
         model.addAttribute("loginErrorMsg", "아이디 또는 비밀번호를 확인해주세요");
