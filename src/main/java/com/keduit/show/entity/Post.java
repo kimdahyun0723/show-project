36fbf13fb45eccbe4a33ff88226146d4dab42783
Moon <bbb0755@gmail.com>
2024-10-10 12:15:44 +0900

    게시판 수정

--- src/main/java/com/keduit/show/entity/Post.java
+++ src/main/java/com/keduit/show/entity/Post.java
@@ -1,13 +1,18 @@
 package com.keduit.show.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+@Setter
+@Getter
 @Entity
 public class Post {
 
+  // Getters and Setters
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
@@ -15,28 +20,4 @@ public class Post {
   private String title;
   private String content;
 
-  // Getters and Setters
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-  public String getContent() {
-    return content;
-  }
-
-  public void setContent(String content) {
-    this.content = content;
-  }
 }
